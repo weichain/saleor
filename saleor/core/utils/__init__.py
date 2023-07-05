33ecf6aa4f17d949a42652d46711c1b5a665add0
Steliyan Nedev <steliyannedev@Steliyans-MacBook-Pro.local>
2023-07-05 15:13:22 +0300

    fixing uri and adding allowed_host

--- saleor/core/utils/__init__.py
+++ saleor/core/utils/__init__.py
@@ -31,7 +31,7 @@ def build_absolute_uri(location: str, domain: Optional[str] = None) -> str:
     host = domain or Site.objects.get_current().domain
     protocol = "https" if settings.ENABLE_SSL else "http"
     current_uri = "%s://%s" % (protocol, host)
-    location = urljoin(current_uri, location)
+    location = urljoin(current_uri, '.' + location)
     return iri_to_uri(location)
 
 
